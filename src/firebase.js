4a5b4a13e0884541019ad0fa66caeb804db27ffb
Amine720 <48015352+Amine720@users.noreply.github.com>
2020-08-30 20:44:22 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbWXmqm_RJIJOeTI6-H8d-UEISBjJB3xk",
-  authDomain: "whatsapp-clone-a295c.firebaseapp.com",
-  databaseURL: "https://whatsapp-clone-a295c.firebaseio.com",
-  projectId: "whatsapp-clone-a295c",
-  storageBucket: "whatsapp-clone-a295c.appspot.com",
-  messagingSenderId: "902394952695",
-  appId: "1:902394952695:web:4cdf80ec6bbfa2b325ccf2",
+  // PUT HERE YOUR CREDENTIALS
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
